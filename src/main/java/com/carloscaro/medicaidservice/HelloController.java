577c922a1b3d949255b02a4989d9b2b5fa552caf
Carlos Caro <ccaro002@gmail.com>
2026-08-25 21:35:30 -0700

    Spring Boot skeleton: hello endpoint running

---
+++ src/main/java/com/carloscaro/medicaidservice/HelloController.java
@@ -0,0 +1,13 @@
+package com.carloscaro.medicaidservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "medicaid-service is up";
+    }
+}
---
+++ src/main/java/com/carloscaro/medicaidservice/MedicaidServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carloscaro.medicaidservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicaidServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicaidServiceApplication.class, args);
+    }
+
+}
